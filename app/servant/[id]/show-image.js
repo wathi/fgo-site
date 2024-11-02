278e0768d56c1ac4eb581f44789d73653675acc7
wathi <68807000+wathi@users.noreply.github.com>
2024-11-02 21:59:14 +0000

    Update select chara figure

--- app/servant/[id]/show-image.js
+++ app/servant/[id]/show-image.js
@@ -6,28 +6,29 @@ export default function ShowImage({ faces, name }) {
   console.log(faces);
   const faceKey = Object.keys(faces).map((key) => key);
   const [currentFaceKey, setCurrentFaceKey] = useState(faceKey[0]);
+  const [isActive, setActive] = useState(false);
+
+  const selectcharaFigure = () => {
+    setActive(true);
+  };
+
   return (
     <div className="mt-10">
       <div className="mb-2">再臨セイントグラフ</div>
       <div className="flex">
-        <div
-          className="px-1 mr-2 border border-sky-500 rounded-md cursor-pointer"
-          onClick={() => setCurrentFaceKey(1)}
-        >
-          第１再臨
-        </div>
-        <div
-          className="px-1 mr-2 border border-sky-500 rounded-md cursor-pointer"
-          onClick={() => setCurrentFaceKey(2)}
-        >
-          第２再臨
-        </div>
-        <div
-          className="px-1 mr-2 border border-sky-500 rounded-md cursor-pointer"
-          onClick={() => setCurrentFaceKey(3)}
-        >
-          第３再臨
-        </div>
+        {faceKey.map((item) => (
+          <div
+            key={item}
+            className={
+              currentFaceKey === item
+                ? 'px-2 py-1 mr-2 border border-sky-600 rounded-md cursor-pointer bg-sky-50'
+                : 'px-2 py-1 mr-2 border border-sky-600 rounded-md cursor-pointer '
+            }
+            onClick={() => setCurrentFaceKey(item)}
+          >
+            第{item}再臨
+          </div>
+        ))}
       </div>
 
       <Image src={faces[currentFaceKey]} width={500} height={500} alt={name} />
